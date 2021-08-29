3a82a764eacb9f5ebc87f4d863bb9958cc947284
biggib01 <77378396+biggib01@users.noreply.github.com>
2021-08-29 13:03:58 +0700

    Add files via upload

---
+++ Controller.java
@@ -0,0 +1,15 @@
+package sample;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+    public void pressButton(ActionEvent event){
+        System.out.println("Noice try.!");
+    }
+
+    public void exit(ActionEvent event){
+        System.exit(0);
+    }
+}
+
+//use for set action of each button
---
+++ Main.java
@@ -0,0 +1,25 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/Image/test.jpg")));
+        primaryStage.setTitle("ADV Programming Project_Prototype01");
+        primaryStage.setScene(new Scene(root, 500, 500));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
